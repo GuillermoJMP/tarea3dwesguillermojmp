3ee79ad820cf25b421a752011ea5af51e86b3a0c
GuillermoJMP <scs82358@educastur.es>
2024-11-26 14:24:12 +0100

    Prueba Inicial.

---
+++ tarea3dwesguillermojmp/src/main/java/com/tarea3dwesguillermojmp/tarea3dwesguillermojmp/principal/Principal.java
@@ -0,0 +1,12 @@
+package com.tarea3dwesguillermojmp.tarea3dwesguillermojmp.principal;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+
+	@Override
+	public void run (String... args)throws Exception{
+		
+		System.out.println("Esto es una prueba.");
+	}
+}
