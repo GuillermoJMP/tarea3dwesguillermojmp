ca72101830a87de0e7970fa04b93199b97f5c2b8
GuillermoJMP <scs82358@educastur.es>
2024-11-26 14:23:51 +0100

    Prueba inicial.

--- tarea3dwesguillermojmp/src/main/java/com/tarea3dwesguillermojmp/tarea3dwesguillermojmp/Tarea3dwesguillermojmpApplication.java
+++ tarea3dwesguillermojmp/src/main/java/com/tarea3dwesguillermojmp/tarea3dwesguillermojmp/Tarea3dwesguillermojmpApplication.java
@@ -1,10 +1,20 @@
 package com.tarea3dwesguillermojmp.tarea3dwesguillermojmp;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tarea3dwesguillermojmp.tarea3dwesguillermojmp.principal.Principal;
 
 @SpringBootApplication
 public class Tarea3dwesguillermojmpApplication {
+	
+	@Bean
+	public Principal  applicationStartupRunner() {
+		return new Principal();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tarea3dwesguillermojmpApplication.class, args);
