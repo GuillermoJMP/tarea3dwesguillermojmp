c86d1ff42614b80379bd886e7e67f1caf6b3da7f
GuillermoJMP <scs82358@educastur.es>
2024-11-26 14:09:48 +0100

    Primer Commit.

---
+++ tarea3dwesguillermojmp/src/main/java/com/tarea3dwesguillermojmp/tarea3dwesguillermojmp/Tarea3dwesguillermojmpApplication.java
@@ -0,0 +1,13 @@
+package com.tarea3dwesguillermojmp.tarea3dwesguillermojmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3dwesguillermojmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3dwesguillermojmpApplication.class, args);
+	}
+
+}
